a407a67cd80b7624bf45d6a7fa9ec3587dbfe860
takecare <rui@vazteixeira.org>
2019-09-08 21:57:01 +0100

    load bricks quads

--- src/Quads.lua
+++ src/Quads.lua
@@ -51,3 +51,33 @@ generateBallsQuads = function (atlas)
 
     return quads
 end
+
+generateBricksQuads = function (atlas)
+    local tilewidth = 32
+    local tileheight = 16
+
+    local sheetWidth = atlas:getWidth() / tilewidth
+    local sheetHeight = atlas:getHeight() / tileheight
+
+    local sheetCounter = 1
+    local spritesheet = {}
+
+    for y = 0, sheetHeight - 1 do
+        for x = 0, sheetWidth - 1 do
+            spritesheet[sheetCounter] = Quad(
+                love.graphics.newQuad(x * tilewidth, y * tileheight, tilewidth, tileheight, atlas:getDimensions()),
+                tilewidth,
+                tileheight
+            )
+            sheetCounter = sheetCounter + 1
+        end
+    end
+
+    local sliced = {}
+  
+    for i = 1 or 1, 21 or #spritesheet, step or 1 do
+      sliced[#sliced+1] = spritesheet[i]
+    end
+  
+    return sliced
+end
