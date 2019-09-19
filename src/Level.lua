8c2731b90609614e5179272500300df48511f625
takecare <rui@vazteixeira.org>
2019-09-19 23:43:29 +0100

    create, render and collide level

--- src/Level.lua
+++ src/Level.lua
@@ -1,20 +1,32 @@
 Level = Class{}
 
+function printTable(table)
+    for i,line in ipairs(table) do
+      for j,v in ipairs(line) do
+        print('('..i..','..j..'): '..v.x..','..v.y)
+      end
+    end
+end
+
 function Level:init(bricks)
     self.bricks = bricks ~= nil and bricks or {}
 end
 
 function Level:render()
-    for i, brick in pairs(self.bricks) do
-        brick:render()
+    for ky, row in pairs(self.bricks) do
+        for kx, brick in pairs(row) do
+            brick:render()
+        end
     end
 end
 
 function Level:collidesWith(object)
-    for i, brick in pairs(self.bricks) do
-        if brick:collidesWith(object) then
-            brick:collidedWith(object:boundingBox())
-            object:collidedWith(brick:boundingBox())
+    for ky, row in pairs(self.bricks) do
+        for kx, brick in pairs(row) do
+            if brick:collidesWith(object) then
+                brick:collidedWith(object:boundingBox())
+                object:collidedWith(brick:boundingBox())
+            end
         end
     end
 end
--- src/LevelMaker.lua
+++ src/LevelMaker.lua
@@ -8,16 +8,39 @@ function LevelMaker:init()
 end
 
 function LevelMaker:createLevel()
-    local x, y = unpack({30, 30})
+    local desiredMargin = 30
+    local x, y = unpack({desiredMargin, 30})
     local brick = Brick(x, y)
-    local bricks = { brick }
+    local bricks = { { brick } }
     local width = brick:boundingBox().width
-    
-    for i = 2, 10 do
-        bricks[i] = Brick(width * (i-1) + x, y)
+
+    local n = math.abs((VIRTUAL_WIDTH - x * 2) / width)
+    local totalWidth = n * x
+    local margin = (VIRTUAL_WIDTH - totalWidth) / 2
+
+    brick.x = margin
+
+    for y = 1, 4 do
+        local row = {}
+        for x = 1, n do
+            table.insert(
+                row,
+                Brick(width * (x-1) + margin,
+                desiredMargin + desiredMargin * (y-1))
+            )
+        end
+        table.insert(bricks, row)
     end
-    
+
     return Level(bricks)
+end
+
+function printTable(table)
+    for i,line in ipairs(table) do
+      for j,v in ipairs(line) do
+        print('('..i..','..j..'): '..v.x..','..v.y)
+      end
+    end
 end
 
 return LevelMaker
