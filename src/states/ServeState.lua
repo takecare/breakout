80d819803a7be3da77391c703ffec01cdae100d4
takecare <rui@vazteixeira.org>
2019-10-04 23:26:38 +0100

    add basic serve state

--- src/states/ServeState.lua
+++ src/states/ServeState.lua
@@ -1,5 +1,31 @@
 local ServeState = Class{__includes = BaseState}
 
--- TODO
+function ServeState:init() 
+    -- TODO create level
+    -- TODO create paddle
+    -- TODO create ball
+end
+
+function ServeState:enter(params) 
+    -- TODO do we need this?
+end
+
+function ServeState:exit() end
+
+function ServeState:update(dt) 
+    -- TODO move paddle across x axis
+    -- TODO move ball across x axis
+end
+
+function ServeState:keyPressed(key) 
+    -- TODO escape pauses
+    -- TODO space serves (move to next state)
+end
+
+function ServeState:render()
+    -- TODO draw bricks
+    -- TODO draw paddle
+    -- TODO draw ball
+end
 
 return ServeState
